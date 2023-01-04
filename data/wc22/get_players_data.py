e24603f5597096c5d8b3130c78083424c5c379c7
vietanhtran2710 <vietanhtran.uet@gmail.com>
2023-01-04 15:35:47 +0700

    fix encoding and update full wc22 data

--- data/wc22/get_players_data.py
+++ data/wc22/get_players_data.py
@@ -14,24 +14,23 @@ import json
 def get_element_value(source, xpath):
     soup = BeautifulSoup(source, "html.parser")
     dom = etree.HTML(str(soup))
-    print(dom.xpath(xpath)[0])
     return dom.xpath(xpath)[0].text
 
 links = []
 players_data = []
 with open("links.txt", "r") as f:
     links = f.readlines()
-for link in links:
+for index, link in enumerate(links):
     player_data = {}
-    firefox = webdriver.Firefox()
-    firefox.get(link)
+    chrome = webdriver.Chrome()
+    chrome.get(link)
     player_data["season"] = "WC22"
-    player_data["name"] = get_element_value(firefox.page_source, '//*[@id="player-info"]/div[1]/div[1]/span')
-    player_data["main_position"] = get_element_value(firefox.page_source, '//*[@id="player-info"]/div[1]/div[2]/span[1]')
-    player_data["country"] = get_element_value(firefox.page_source, '//*[@id="player-info"]/div[1]/div[3]/a/span')
-    player_data["salary"] = get_element_value(firefox.page_source, '//*[@id="player-info"]/div[1]/div[4]/div[1]/div/div')
+    player_data["name"] = get_element_value(chrome.page_source, '//*[@id="player-info"]/div[1]/div[1]/span')
+    player_data["main_position"] = get_element_value(chrome.page_source, '//*[@id="player-info"]/div[1]/div[2]/span[1]')
+    player_data["country"] = get_element_value(chrome.page_source, '//*[@id="player-info"]/div[1]/div[3]/a/span')
+    player_data["salary"] = get_element_value(chrome.page_source, '//*[@id="player-info"]/div[1]/div[4]/div[1]/div/div')
 
-    soup = BeautifulSoup(firefox.page_source, "html.parser")
+    soup = BeautifulSoup(chrome.page_source, "html.parser")
     img = soup.find("img", {"class": "img-fluid i-avatar"})
     img_src = img["src"]
     img_name = img_src.split("/")[-1].split("?")[0]
@@ -39,13 +38,16 @@ for link in links:
         f.write(requests.get(img_src).content)
     player_data["image_name"] = img_name
 
+    player_data["id"] = img_name.split(".")[0]
+
     player_data["stat"] = {}
     for level in range(1, 11):
-        firefox.find_element(By.XPATH, "//*[@id=\"player-info\"]/div[2]/div/div[2]/div[3]/button").click()
-        firefox.find_element(By.XPATH, "//*[@id=\"player-info\"]/div[2]/div/div[2]/div[3]/ul/li[" + str(level) + "]/a").click()
-        player_data["stat"][level] = get_element_value(firefox.page_source, '//*[@id="player-info"]/div[1]/div[2]/span[2]')
+        chrome.find_element(By.XPATH, "//*[@id=\"player-info\"]/div[2]/div/div[2]/div[3]/button").click()
+        chrome.find_element(By.XPATH, "//*[@id=\"player-info\"]/div[2]/div/div[2]/div[3]/ul/li[" + str(level) + "]/a").click()
+        player_data["stat"][level] = get_element_value(chrome.page_source, '//*[@id="player-info"]/div[1]/div[2]/span[2]')
     players_data.append(player_data)
-    firefox.close()
+    chrome.close()
+    print(index, "/", len(links))
 
-with open("wc22.json", "w") as final:
-   json.dump(players_data, final, indent=2)
+with open("wc22.json", "w", encoding='utf-8') as final:
+   json.dump(players_data, final, indent=2, ensure_ascii=False)
