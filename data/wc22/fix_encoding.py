613fa6f03ffba20c87dceef59f15d1cbc058fa4c
vietanhtran2710 <vietanhtran.uet@gmail.com>
2023-01-05 16:34:36 +0700

    remove old scripts

--- data/wc22/fix_encoding.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("wc22.json") as json_file:
-    data = json.load(json_file)
-
-with open("wc22.json", "w", encoding='utf-8') as fixed:
-    json.dump(data, fixed, indent=2, ensure_ascii=False)
